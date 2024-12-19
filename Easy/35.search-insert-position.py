5d0518c929363dc58e6077c0f114fe27267b5c29
aidenjsevans <aidenjsevans@gmail.com>
2024-12-19 13:17:03 +0000

    Create 35.search-insert-position.py

---
+++ Easy/35.search-insert-position.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=35 lang=python3
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+            
+        return low
+        
+# @lc code=end
+
